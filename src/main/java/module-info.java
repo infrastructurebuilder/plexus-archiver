e933676c73f156de43448d24e216e2ded9d8034a
Mykel Alvis <mykel.alvis@gmail.com>
2023-12-28 07:25:43 -0600

    Usurped and made a module
    The tests won't run.  Don't even try.
    
    mvn spotless:apply && mvn -DskipTests=true -Pplexus-release clean
    install

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,7 +1,11 @@
-open module org.codehaus.plexus.archiver.usurped {
+module org.codehaus.plexus.archiver.usurped {
+    exports org.codehaus.plexus.archiver;
+    exports org.codehaus.plexus.archiver.manager;
+    exports org.codehaus.plexus.archiver.zip;
+
     requires javax.inject;
     //    requires io.avaje.jsr305x;
-    requires jsr305;
+    requires io.avaje.jsr305x;
     requires plexus.io;
     requires plexus.utils;
     requires org.apache.commons.compress;
